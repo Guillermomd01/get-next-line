0db90205a08a0bd44ea16c494f79a10ff972c2e1
Guillermo Munoz <gumunoz@c2r17s2.42madrid.com>
2025-10-23 18:52:04 +0200

    Buffer size fixed

--- get_next_line.h
+++ get_next_line.h
@@ -1,16 +1,29 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gumunoz <gumunoz@student.42madrid.com      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/21 15:37:44 by gumunoz           #+#    #+#             */
+/*   Updated: 2025/10/21 15:37:57 by gumunoz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFFER_SIZE 42
 
-#include <unistd.h>
-#include <stdlib.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+
+#endif
+
+# include <unistd.h>
+# include <stdlib.h>
 # include <fcntl.h>
 # include <stddef.h>
 
-char *get_next_line(int fd);
-
+char	*get_next_line(int fd);
 
 /*utils funtions*/
 char	*ft_strchr(const char *s, int c);
@@ -18,4 +31,8 @@ size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+char	*leftover_line(char *buffer);
+char	*extract_line(char *buffer);
+char	*read_until_newline(int fd, char *buffer);
 #endif
